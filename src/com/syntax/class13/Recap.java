eb36f9661ffce763113d1010a98fe479aadd7e32
[name] <umedkasymov1@gmail.com>
2020-03-18 20:37:15 -0400

    Q1 was modified

--- src/com/syntax/class13/Recap.java
+++ src/com/syntax/class13/Recap.java
@@ -3,7 +3,16 @@ package com.syntax.class13;
 public class Recap {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// String is a class that has its own methods
+		//convert String from upper to lower case
+		//extract substring from a String
+		//Replace character/characters
+		//split FUNC it is going to cau our String into many many String
+		//...and return us an Array String
+		//gitHub- remote repository
+		//GIT-version control tool
+		//
+		
 
 	}
 
