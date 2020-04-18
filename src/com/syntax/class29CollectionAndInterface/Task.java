125cc00f21a9279e86ea9526adbacb6e7418ecfd
[name] <umedkasymov1@gmail.com>
2020-04-18 12:26:55 -0400

    New Class of Collection was Added

---
+++ src/com/syntax/class29CollectionAndInterface/Task.java
@@ -0,0 +1,17 @@
+package com.syntax.class29CollectionAndInterface;
+
+public class Task {
+
+	/*
+	 * Create a Set collection in which you need to add names of the countries. In
+	 * this set we want all objects to be sorted in alphabetical order. Using 2
+	 * different ways retrieve all elements from set. Create a Set of cities in
+	 * which you want to make sure that insertion order is maintained. Using
+	 * Iterator remove any city that starts with “A”; Create a Set collection that
+	 * will hold Objects of Student Type. In this set we do not care about the
+	 * insertion order. Each student object should have name and studentID. Display
+	 * name of each student.
+	 * 
+	 */
+	
+}
